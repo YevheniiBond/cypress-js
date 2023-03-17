76bd97874ed413ffeae5a642a63a0c7936a1c673
fdgfgdf <inrterdancecoach@gmail.com>
2023-03-17 17:58:06 +0200

    settup for cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const  { defineConfig } = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('file:preprocessor', cucumber()) // implement node event listeners here
     }, 
     specPattern: "cypress/e2e/**/*.{js,ts,feature}",
     baseUrl:"http://www.webdriveruniversity.com",
