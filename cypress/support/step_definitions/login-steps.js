ecce5d3e9f0ee00746a804afac131a7db0bd3dc3
fdgfgdf <inrterdancecoach@gmail.com>
2023-03-17 18:40:09 +0200

    login.feature working fine

--- cypress/support/step_definitions/login-steps.js
+++ cypress/support/step_definitions/login-steps.js
@@ -7,3 +7,15 @@ Given('I access the WebdriverUniversity Login Portal page', () => {
 When("I enter a username {word}", (userName) =>{
     cy.get("#text").type(userName);
 })
+
+And("I enter a password {word}", (userName) =>{
+    cy.get("#password").type(userName);
+})
+
+And('I click on the login button', () => {
+    cy.get("#login-button").click();
+})
+
+Then('I should be presented with the following message {word} {word}', (message, message2) => {
+    
+})
