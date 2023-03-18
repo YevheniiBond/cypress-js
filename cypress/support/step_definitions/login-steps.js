00f8c5092a166bbf8b71d38d04ad29bd6e396007
fdgfgdf <inrterdancecoach@gmail.com>
2023-03-18 07:15:09 +0200

    test pass

--- cypress/support/step_definitions/login-steps.js
+++ cypress/support/step_definitions/login-steps.js
@@ -1,5 +1,12 @@
 import { Before, Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
+let stub;
+
+Before(() => {
+    cy.log("Executing before step");
+    stub = cy.stub();
+})
+
 Given('I access the WebdriverUniversity Login Portal page', () => {
     cy.visit("http://www.webdriveruniversity.com/Login-Portal/index.html");
 })
@@ -14,8 +21,12 @@ And("I enter a password {word}", (userName) =>{
 
 And('I click on the login button', () => {
     cy.get("#login-button").click();
+    cy.on('window:alert', stub);
 })
 
 Then('I should be presented with the following message {word} {word}', (message, message2) => {
-    
+    const expectedMessage = message + " " + message2;
+    cy.log(expectedMessage);
+    cy.log(stub.getCall(0));
+    expect(stub.getCall(0)).to.be.calledWith(expectedMessage); 
 })
